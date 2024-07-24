cc4e0a7f600c6d6f772c5bd504d2e907ddf949b3
Liam Loughead <64972906+Snapwhiz914@users.noreply.github.com>
2024-07-24 08:16:09 -0400

    Add auto routine
    Run till magnet, turn and get it, turn around and go back the way it came 3 times (Still not compiled lol)

--- src/CompCode/CompCode.ino
+++ src/CompCode/CompCode.ino
@@ -13,6 +13,7 @@ Pixy2I2C pixy;
 Drive drive;
 Arm arm;
 Gripper gripper;
+int gameObjectsCollected = 0;
 
 /*********************************** Setup ************************************/
 
@@ -25,8 +26,41 @@ void setup() {
   Serial.println("Subsystems set up");
   //Game auto sequence can go here
   //Remember to use the non blocking feature to do things simultaneously!
+  while(gameObjectsCollected < 3) {
+    drive.follow_line_til_mag(True);
+    drive.turn(90);
+    int bcid = -1;
+    while (bcid == -1) {
+      drive.follow_line_til_mag(False);
+      arm.run_motor(ChestArmPosition);
+      bcid = drive.get_visible_bcid();
+    }
+    int arm_pos = get_arm_position_for(bcid);
+    while (!drive.center_barcode(bcid, false)) {
+      arm.run_motor(arm_pos);
+    }
+    while (!arm.run_motor(arm_pos)) {}
+    gripper.close(true);
+    delay(250);
+    drive.turn(180);
+    while (drive.follow_line_til_mag(False)) {
+      arm.run_motor(IdleArmHeight);
+    }
+    drive.turn(-90);
+    drive.follow_line_til_mag(true);
+    gripper.open();
+    drive.turn(180);
+    gameObjectsCollected++;
+  }
 }
 
+int get_arm_position_for(int bcid) {
+  switch (bcid) {
+    case ChestBarcodeID: return ChestArmPosition;
+    case PineappleBarcodeID: return PineappleArmPosition;
+    case TridentBarcodeID: return TridentArmPosition;
+  }
+}
 
 /************************************ Loop ************************************/
 
